e59d706b6e36b6f88538ace8ce8483f434f112cf
TomHollinberger <tom.hollinberger@gmail.com>
2020-09-28 12:06:28 -0500

    Committing Task 9

---
+++ DTS350TemplateMaster/Week_05/Class_Task_09/9Task.R
@@ -0,0 +1,41 @@
+#' ---
+#' title: "Task 9: Self Evaluation"
+#' author: "TomHollinberger"
+#' date: "9/29/2020"
+#' output: 
+#'  html_document: 
+#'    keep_md: yes
+#'    toc: TRUE
+#' ---  
+#' THIS RSCRIPT USES ROXYGEN CHARACTERS.  
+#' YOU CAN PRESS ctrl+shift+K AND GO STRAIGHT TO A HTML.  
+#' SKIPS THE HANDWORK OF CREATING A RMD, AFTER THE ORIGINAL WORK IS NONE IN A RSCRIPT.
+#'
+#'# **Week 5, Task 9 : Self-Evaluation** 
+#'
+#' _________________________________
+#'   In an .Rmd file write a self-evaluation of this course.
+#'	[ ] Include a section that explains your strengths in the class.
+#'	[ ] Include a section that explains areas you could improve on.
+#'	[ ] Include a section that lists your goals for the course and your plan to achieve those goals.
+#'
+#' _________________________________
+#
+#'
+#'## Strengths in the class.
+#' 1. Persistence
+#' 2. Makes time available to dedicate to coding.
+#' 3. Ability to think graphically, read charts, and know what constitutes good graphics to typcal viewers.
+#' 4. Good documentation, class notes, hardcopy binder of examples.
+#'
+#'
+#' _________________________________
+#'
+#'## Areas I could improve on.
+#' 1. More efficient coding, using pipes, not using step-by-step build ups of code.
+#'
+#'
+#'## Goals for the course and your plan to achieve those goals
+#' 1.  I want to not fall behind.  It takes me a long time to code, so I need to get ahead and stay ahead as long as possible.
+#' 2.  Streamline my coding, to be more efficient. USe pipes and global settings.
+#' 3.  Somehow turn the rmds & htmls into classnotes, examples to use for future reference.  
